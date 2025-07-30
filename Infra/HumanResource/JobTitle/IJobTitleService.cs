d33117d3607f0a3c479ec657400ad5a59444b025
Felipe Machado <felipe.barcellos.machado@gmail.com>
2025-07-30 17:12:17 -0300

    JubTitles

--- Infra/HumanResource/JobTitle/IJobTitleService.cs
+++ Infra/HumanResource/JobTitle/IJobTitleService.cs
@@ -4,5 +4,6 @@ namespace Workforce.Services.Infra.HumanResource.JobTitle
 {
     public interface IJobTitleService : ICrudService<Domain.Infra.HumanResource.JobTitle.Entity.JobTitle>
     {
+        Task<IList<Domain.Infra.HumanResource.JobTitle.Entity.JobTitle>> GetAllByEnvironmentId(int environmentId);
     }
 }
