a98a356bc12009067eb088b13608068e350ee67a
Felipe Machado <felipe.barcellos.machado@gmail.com>
2025-08-08 07:20:44 -0300

    Ajustes

--- ICrudService.cs
+++ ICrudService.cs
@@ -7,5 +7,11 @@
         Task<IList<T>> GetAll();
         Task<T> Update(object dto);
         Task Delete(int id);
+        Task<T> InsertAsync(object dto);
+        Task<T> GetByIdAsync(int id);
+        Task<IList<T>> GetAllAsync();
+        Task<T> UpdateAsync(object dto);
+        Task DeleteAsync(int id);
+
     }
 }
