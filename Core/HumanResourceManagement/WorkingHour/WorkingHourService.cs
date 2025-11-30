383ced161c105c699675fae843991078194890b1
Felipe Machado <felipe.barcellos.machado@gmail.com>
2025-11-29 21:31:40 -0300

    Ajustes

--- Core/HumanResourceManagement/WorkingHour/WorkingHourService.cs
+++ Core/HumanResourceManagement/WorkingHour/WorkingHourService.cs
@@ -2,20 +2,20 @@ using System.Net.Http.Json;
 
 namespace Workforce.Services.Core.HumanResourceManagement.WorkingHour
 {
-    public class WorkingHourService : CrudService<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingHour>, IWorkingHourService
+    public class WorkingHourService : CrudService<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingTime>, IWorkingHourService
     {
         public WorkingHourService(HttpClient httpClient) : base(httpClient, "api/core/human_resource/workinghour")
         {
         }
 
-        public async Task<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingHour> GetByEnvironmentIdAndId(int environmentId, int id)
+        public async Task<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingTime> GetByEnvironmentIdAndId(int environmentId, int id)
         {
             try
             {
                 var response = await _httpClient.GetAsync($"{_baseUri}/environment/{environmentId}/{id}");
                 if (response.IsSuccessStatusCode)
                 {
-                    var result = await response.Content.ReadFromJsonAsync<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingHour>(_jsonOptions);
+                    var result = await response.Content.ReadFromJsonAsync<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingTime>(_jsonOptions);
                     return result!;
                 }
                 return null!;
@@ -27,15 +27,15 @@ namespace Workforce.Services.Core.HumanResourceManagement.WorkingHour
             }
         }
 
-        public async Task<IList<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingHour>> GetAllByEnvironmentId(int environmentId)
+        public async Task<IList<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingTime>> GetAllByEnvironmentId(int environmentId)
         {
             try
             {
                 var response = await _httpClient.GetAsync($"{_baseUri}/all/environment/{environmentId}");
                 response.EnsureSuccessStatusCode();
 
-                var result = await response.Content.ReadFromJsonAsync<IList<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingHour>>(_jsonOptions);
-                return result ?? new List<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingHour>();
+                var result = await response.Content.ReadFromJsonAsync<IList<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingTime>>(_jsonOptions);
+                return result ?? new List<Domain.Core.HumanResourceManagement.WorkingHour.Entity.WorkingTime>();
             }
             catch (Exception ex)
             {
