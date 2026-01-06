dc65722ac8e4404e26ff21a35e262123c98c6f54
Felipe Machado <felipe.barcellos.machado@gmail.com>
2026-01-06 06:35:02 -0300

    Ajustes

--- Core/HumanResourceManagement/WorkingTime/IWorkingTimeService.cs
+++ Core/HumanResourceManagement/WorkingTime/IWorkingTimeService.cs
@@ -3,9 +3,9 @@ using System.Threading.Tasks;
 
 namespace Workforce.Services.Core.HumanResourceManagement.WorkingTime
 {
-    public interface IWorkingTimeService : ICrudService<Domain.Core.HumanResourceManagement.WorkingTime.Entity.WorkingTime>
+    public interface IWorkingTimeService : ICrudService<Domain.Core.TourScheduleManagement.WorkingTime.Entity.WorkingTime>
     {
-        Task<Domain.Core.HumanResourceManagement.WorkingTime.Entity.WorkingTime> GetByEnvironmentIdAndId(int environmentId, int id);
-        Task<IList<Domain.Core.HumanResourceManagement.WorkingTime.Entity.WorkingTime>> GetAllByEnvironmentId(int environmentId);
+        Task<Domain.Core.TourScheduleManagement.WorkingTime.Entity.WorkingTime> GetByEnvironmentIdAndId(int environmentId, int id);
+        Task<IList<Domain.Core.TourScheduleManagement.WorkingTime.Entity.WorkingTime>> GetAllByEnvironmentId(int environmentId);
     }
 }
