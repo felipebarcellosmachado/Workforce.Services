21bfd16369278b79aeb919ded71bf6d8aac17478
Felipe Machado <felipe.barcellos.machado@gmail.com>
2026-01-28 11:47:04 -0300

    ÁJustes em RiskFactor

--- Core/HumanResourceManagement/RiskFactor/IRiskFactorService.cs
+++ Core/HumanResourceManagement/RiskFactor/IRiskFactorService.cs
@@ -4,5 +4,6 @@ namespace Workforce.Services.Core.HumanResourceManagement.RiskFactor
 {
     public interface IRiskFactorService : ICrudService<Domain.Core.HumanResourceManagement.RiskFactor.Entity.RiskFactor>
     {
+        Task<IList<Domain.Core.HumanResourceManagement.RiskFactor.Entity.RiskFactor>> GetAllByEnvironmentIdAsync(int environmentId);
     }
 }
--- Core/HumanResourceManagement/RiskFactor/RiskFactorService.cs
+++ Core/HumanResourceManagement/RiskFactor/RiskFactorService.cs
@@ -8,5 +8,13 @@ namespace Workforce.Services.Core.HumanResourceManagement.RiskFactor
         public RiskFactorService(HttpClient httpClient) : base(httpClient, "api/core/human-resource-management/riskfactor")
         {
         }
+
+        public async Task<IList<Domain.Core.HumanResourceManagement.RiskFactor.Entity.RiskFactor>> GetAllByEnvironmentIdAsync(int environmentId)
+        {
+            var response = await _httpClient.GetAsync($"{_baseUri}/all/environment/{environmentId}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<IList<Domain.Core.HumanResourceManagement.RiskFactor.Entity.RiskFactor>>(_jsonOptions) 
+                   ?? new List<Domain.Core.HumanResourceManagement.RiskFactor.Entity.RiskFactor>();
+        }
     }
 }
