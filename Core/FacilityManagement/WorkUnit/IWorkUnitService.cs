90210e98034b609fdb2372514360691ea7e5a94a
Felipe Machado <felipe.barcellos.machado@gmail.com>
2026-02-04 09:15:52 -0300

    Ajustes

--- Core/FacilityManagement/WorkUnit/IWorkUnitService.cs
+++ Core/FacilityManagement/WorkUnit/IWorkUnitService.cs
@@ -2,5 +2,6 @@ namespace Workforce.Services.Core.FacilityManagement.WorkUnit
 {
     public interface IWorkUnitService : ICrudService<Domain.Core.FacilityManagement.WorkUnit.Entity.WorkUnit>
     {
+        Task<IList<Domain.Core.FacilityManagement.WorkUnit.Entity.WorkUnit>> GetAllByEnvironmentId(int environmentId);
     }
 }
--- Core/FacilityManagement/WorkUnit/WorkUnitService.cs
+++ Core/FacilityManagement/WorkUnit/WorkUnitService.cs
@@ -1,4 +1,5 @@
 using System.Net.Http;
+using System.Net.Http.Json;
 using Workforce.Services.Infra;
 
 namespace Workforce.Services.Core.FacilityManagement.WorkUnit
@@ -7,6 +8,26 @@ namespace Workforce.Services.Core.FacilityManagement.WorkUnit
     {
         public WorkUnitService(HttpClient httpClient) : base(httpClient, "api/core/workunit")
         {
+        }
+
+        public async Task<IList<Workforce.Domain.Core.FacilityManagement.WorkUnit.Entity.WorkUnit>> GetAllByEnvironmentId(int environmentId)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"{_baseUri}/environment/{environmentId}");
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadFromJsonAsync<IList<Workforce.Domain.Core.FacilityManagement.WorkUnit.Entity.WorkUnit>>() ?? new List<Workforce.Domain.Core.FacilityManagement.WorkUnit.Entity.WorkUnit>();
+                }
+                else
+                {
+                    throw new Exception($"Error fetching work units: {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while fetching work units: {ex.Message}", ex);
+            }
         }
     }
 }
